f9e54c3db8d383316189b204a17dd8e28d263b34
Bekt <bekt17@gmail.com>
2014-12-02 16:10:43 -0600

    Travis and deployment script.

---
+++ bin/deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# This is a very simple script that deploys to App Engine.
+# No validation is performed whatsoever.
+
+GAE=$HOME/google_appengine/
+
+# GAE SDK.
+function gae_deps {
+  if [ ! -d "$GAE" ]; then
+     echo '> Downloading App Engine SDK...'
+     curl -o $HOME/gae.zip https://storage.googleapis.com/appengine-sdks/featured/google_appengine_1.9.15.zip
+     unzip -q -d $HOME $HOME/gae.zip
+  fi
+}
+
+function deploy {
+  # Try to detect if appcfg.py is in the path.
+  appcfg=$(which appcfg.py)
+  if [ ! "$appcfg" ]; then
+    gae_deps
+  else
+    echo '> GAE already exists.'
+    # Update GAE path if appcfg already exists.
+    GAE=$(dirname $appcfg)
+  fi
+  $GAE/appcfg.py -v --oauth2_refresh_token=$GAE_OAUTH_TOKEN update .
+  echo "> Deployed."
+}
+
+deploy
