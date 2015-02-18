ccad735611d4d25873f0360b63e5753b01d89a68
Bekt <bekt17@gmail.com>
2015-02-17 20:32:58 -0600

    App Engine libs and updated handlers.

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import webapp2
 
 
 app = webapp2.WSGIApplication([
-    webapp2.Route(r'/api/echo', name='api_echo', methods=['GET'],
+    webapp2.Route(r'/api/echo', name='echo', methods=['GET'],
                  handler='controllers.ApiHandler:echo'),
+    webapp2.Route(r'/api/enqueue', name='enqueue', methods=['POST'],
+                 handler='controllers.ApiHandler:enqueue'),
+    webapp2.Route(r'/api/result', name='result', methods=['GET'],
+                 handler='controllers.ApiHandler:result'),
+
+    webapp2.Route(r'/queue/pop', name='pop', methods=['POST'],
+                 handler='tqueue.QueueHandler:pop'),
 ], debug=True)
